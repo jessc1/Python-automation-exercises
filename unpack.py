6f8a703da15a75fdc1c605bb75215dc7d83b2bac
jessc1 <mmariajessica@gmail.com>
2020-11-23 14:58:13 -0300

    writing a pdf document

---
+++ unpack.py
@@ -0,0 +1,9 @@
+def bubble_sort(a):
+    for _ in range(len(a)):
+        for i in range(1, len(a)):
+            if a[i] < a[i-1]:
+                a[i-1], a[i] = a[i], a[i-1]
+
+numbers = [3,2,0,4]
+bubble_sort(numbers)
+print(numbers)
